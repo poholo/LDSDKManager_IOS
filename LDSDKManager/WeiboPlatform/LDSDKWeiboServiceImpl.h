e850ca959856e430309bb8304a2da151b74e9690
poholo <675936746@qq.com>
2018-12-01 10:53:26 +0800

    pod spec classes

---
+++ LDSDKManager/WeiboPlatform/LDSDKWeiboServiceImpl.h
@@ -0,0 +1,14 @@
+//
+//  LDSDKWeiboServiceImpl.h
+//  LDSDKManager
+//
+//  Created by ss on 15/9/1.
+//  Copyright (c) 2015年 张海洋. All rights reserved.
+//
+
+#import "LDSDKRegisterService.h"
+#import "LDSDKShareService.h"
+
+@interface LDSDKWeiboServiceImpl : NSObject <LDSDKRegisterService, LDSDKShareService>
+
+@end
